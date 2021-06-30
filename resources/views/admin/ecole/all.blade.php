34fa089a9b229e8694f8104ed2fe11bba40441f6
Kossonou <kossonou93@hotmail.fr>
2021-06-30 15:41:28 +0000

    --- Auto-Git Commit ---

--- resources/views/admin/ecole/all.blade.php
+++ resources/views/admin/ecole/all.blade.php
@@ -11,7 +11,8 @@
 				<div class="page-inner">
 					@include('admin/part/menu1')
 					<div class="row">
-						<div class="col-md-8">
+						<div class="col-md-12
+						">
 							<div class="card">
 								<div class="card-header">
 									<div class="d-flex align-items-center">
@@ -55,28 +56,6 @@
 											</tbody>
 										</table>
 									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-4">
-							<div class="card card-secondary">
-								<div class="card-header">
-									<div class="card-title">Daily Sales</div>
-									<div class="card-category">March 25 - April 02</div>
-								</div>
-								<div class="card-body pb-0">
-									<div class="mb-4 mt-2">
-										<h1>$4,578.58</h1>
-									</div>
-									<div class="pull-in">
-										<canvas id="dailySalesChart"></canvas>
-									</div>
-								</div>
-							</div>
-							<div class="card card-info bg-info-gradient">
-								<div class="card-body">
-									<h4 class="mb-1 fw-bold">Tasks Progress</h4>
-									<div id="task-complete" class="chart-circle mt-4 mb-3"></div>
 								</div>
 							</div>
 						</div>
