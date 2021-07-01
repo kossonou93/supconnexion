48ce6587656b898d08ec089aba86ce7c4926b913
Kossonou <kossonou93@hotmail.fr>
2021-07-01 14:42:25 +0000

    --- Auto-Git Commit ---

--- resources/views/parts/carousel.blade.php
+++ resources/views/parts/carousel.blade.php
@@ -5,9 +5,9 @@
             <div data-thumb="admini/image_carousel/ste_lucie.jpg" data-src="{{ asset('uploads/photo/carousel/'.$carousel->photo) }}">
                 <div class="camera_caption">
                    <div class="container">
-                        <h4 class=" wow fadeInUp animated" style="font-family: 'Comic Sans MS'; font-weight: bold; background-color: rgb(0, 0, 0, 0.2); padding: 20px, 0px, 20px, 0px; width: 90%;  margin-right:5%; margin-left:5%; text-align:center; font-style: italic; color: #FFFFFF">{{ $carousel->titre }}</h4>
+                        <h1 class=" wow fadeInUp animated titre-carousel">{{ $carousel->titre }}</h1>
                         <p class=" wow fadeInUp animated" data-wow-delay="0.8s" style="color: #f6b60b"><i class="fa fa-hand-o-down fa-4x" aria-hidden="true"></i></p>
-                        <a class=" wow fadeInUp animated" data-wow-delay="1s" href="{{route('ecole.register')}}">En savoir plus</a>
+                        <a class=" wow fadeInUp animated" data-wow-delay="1s" href="{{route('leprojet')}}">En savoir plus</a>
                    </div>
                 </div>
             </div>
