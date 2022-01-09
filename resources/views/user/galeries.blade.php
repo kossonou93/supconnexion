55d68bf081c1266d38d11dd08c698cc958620ab4
DESKTOP-MAKFBMP\MARTIN KOSSONOU <martin.kossonou@ictgency.com>
2022-01-09 21:10:29 +0100

    --- Auto-Git Commit ---

--- resources/views/user/galeries.blade.php
+++ resources/views/user/galeries.blade.php
@@ -16,6 +16,40 @@
             </ol>
         </section>
         <!-- End Banner area -->
+        <div class="row">
+            <div class="card mt-3 col-lg-3 col-md-4 col-sm-6 offset-sm-0 offset-1 col-10" style="width: 18rem;">
+                <img class="card-img-top mt-3" src="assets/m1.png" class="card-img-top">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Pierre Leclercq</h5>
+                    <p class="card-text"><small class="text-muted">Arrivé le 07/02/2011</small></p>
+                </div>
+            </div>
+
+            <div class="card mt-3 col-lg-3 col-md-4 col-sm-6 offset-sm-0 offset-1 col-10" style="width: 18rem;">
+                <img class="card-img-top mt-3" src="users/images/logo.png" class="card-img-top">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Justine Fabvre</h5>
+                    <p class="card-text"><small class="text-muted">Arrivé le 11/06/2012</small></p>
+                </div>
+            </div>
+
+            <div class="card mt-3 col-lg-3 col-md-4 col-sm-6 offset-sm-0 offset-1 col-10" style="width: 18rem;">
+                <img class="card-img-top mt-3" src="users/images/logo.png" class="card-img-top">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Marine Lavoie</h5>
+                    <p class="card-text"><small class="text-muted">Arrivé le 05/12/2016</small></p>
+                </div>
+            </div>
+
+            <div class="card mt-3 col-lg-3 col-md-4 col-sm-6 offset-sm-0 offset-1 col-10" style="width: 18rem;">
+                <img class="card-img-top mt-3" src="users/images/logo.png" class="card-img-top">
+                <div class="card-body text-center">
+                    <h5 class="card-title">Fanny Chapin</h5>
+                    <p class="card-text"><small class="text-muted">Arrivé le 02/05/2017</small></p>
+                </div>
+            </div>
+
+        </div>
         <!-- All contact Info -->
         <section class="all_contact_info">
         <div class="container">
