439f796f248f0b5ddbb935907e55f04e87d98a5f
DESKTOP-MAKFBMP\MARTIN KOSSONOU <martin.kossonou@ictgency.com>
2022-02-11 00:03:06 +0100

    --- Auto-Git Commit ---

---
+++ app/Http/Controllers/LocalizationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LocalizationController extends Controller
+{
+    public function setLang($locale){
+        
+    }
+}
