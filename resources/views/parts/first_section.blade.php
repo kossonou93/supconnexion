d0c720f2747ab267078fdc098f06448f5494dbd2
DESKTOP-MAKFBMP\MARTIN KOSSONOU <martin.kossonou@ictgency.com>
2022-02-13 19:34:39 +0100

    --- Auto-Git Commit ---

--- resources/views/parts/first_section.blade.php
+++ resources/views/parts/first_section.blade.php
@@ -9,17 +9,9 @@
                 </a>
             </li>
         </ul>
-        <ul class="nav navbar-nav navbar-right social_nav">
-            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right" style="margin-right: -350px">
-            <li><a type="button" href="locale/fr" class="btn-image white-text" style="background-image: url('users/images/fr.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px; margin-right: 5px"></a></li>
-            <li><a type="button" href="locale/en" class="btn-image white-text" style="background-image: url('users/images/en.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px"></a></li>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="locale/fr" style="color: #f6b60b; font-weight: bold">FRENCH</a></li>
+            <li><a href="locale/en" style="color: #f6b60b; font-weight: bold">ENGLISH</a></li>
         </ul>
     </div>
 </section>
--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -2,7 +2,34 @@
 
     @include('parts/first_section')
 
-	<!-- Header_Area -->
+<!--<section class="top_header_area">
+    <div class="container">
+        <ul class="nav navbar-nav top_nav">
+            <li><a href="#"><i class="fa fa-envelope-o"></i>contact@sup-connexion.com</a></li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-clock-o"></i>
+                    <span id="date_heure"></span>
+                </a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right social_nav">
+            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right" style="margin-right: -350px">
+            <li><a type="button" href="locale/fr" class="btn-image white-text" style="background-image: url('users/images/fr.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px; margin-right: 5px"></a></li>
+            <li><a type="button" href="locale/en" class="btn-image white-text" style="background-image: url('users/images/en.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px"></a></li>
+        </ul>
+    </div>
+</section>
+
+
+	 Header_Area -->
     <nav class="navbar navbar-default header_aera" id="main_navbar">
         <div class="container">
             <div class="col-md-2 p0">
@@ -34,24 +61,24 @@
                         <li class="dropdown submenu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">@lang('public.inscription')</a>
                             <ul class="dropdown-menu">
-                                <li><a href="{{route('ecole.register')}}">Inscription Etablissement</a></li>
-                                <li><a href="{{route('intervenant.register')}}">Inscription Intervenant</a></li>
+                                <li><a href="{{route('ecole.register')}}">@lang('public.inscriptionEts')</a></li>
+                                <li><a href="{{route('intervenant.register')}}">@lang('public.inscriptionInt')</a></li>
                             </ul>
                         </li>
                         <li><a href="#"></a></li>
                         <li class="dropdown submenu">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Connexion</a>
                             <ul class="dropdown-menu">
-                                <li><a href="{{route('ecole.login')}}">Connexion Etablissement</a></li>
-                                <li><a href="{{route('intervenant.login')}}">Connexion Intervenant</a></li>
+                                <li><a href="{{route('ecole.login')}}">@lang('public.connexionEts')</a></li>
+                                <li><a href="{{route('intervenant.login')}}">@lang('public.connexionInt')</a></li>
                             </ul>
                         </li>
                         
                       	<li class="dropdown submenu">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">A Propos</a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">@lang('public.apropos')</a>
                           	<ul class="dropdown-menu">
-                              <li><a href="{{route('leprojet')}}">Le projet Sup'Connexion</a></li>
-                              <li><a href="{{route('condition.generale')}}">Conditions générales de ventes</a></li>
+                              <li><a href="{{route('leprojet')}}">@lang('public.projetSup')</a></li>
+                              <li><a href="{{route('condition.generale')}}">@lang('public.conditionGle')</a></li>
                               <li><a href="#">Charte utilisateurs</a></li>
                               <li><a href="#">Protection des données</a></li>
                               <li><a href="{{route('actualite.index')}}">Actualités dans le Supérieur</a></li>
