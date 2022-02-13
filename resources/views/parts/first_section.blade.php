397535ce37de05cdd9c620e9a5e40549b7325950
DESKTOP-MAKFBMP\MARTIN KOSSONOU <martin.kossonou@ictgency.com>
2022-02-13 19:04:16 +0100

    --- Auto-Git Commit ---

---
+++ resources/views/parts/first_section.blade.php
@@ -0,0 +1,25 @@
+<section class="top_header_area">
+    <div class="container">
+        <ul class="nav navbar-nav top_nav">
+            <li><a href="#"><i class="fa fa-envelope-o"></i>contact@sup-connexion.com</a></li>
+            <li>
+                <a href="#">
+                    <i class="fa fa-clock-o"></i>
+                    <span id="date_heure"></span>
+                </a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right social_nav">
+            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right" style="margin-right: -350px">
+            <li><a type="button" href="locale/fr" class="btn-image white-text" style="background-image: url('users/images/fr.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px; margin-right: 5px"></a></li>
+            <li><a type="button" href="locale/en" class="btn-image white-text" style="background-image: url('users/images/en.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px"></a></li>
+        </ul>
+    </div>
+</section>
--- resources/views/parts/menu.blade.php
+++ resources/views/parts/menu.blade.php
@@ -1,50 +1,6 @@
 <div id="google_translate_element" style="display: none"></div>
 
-<section class="top_header_area">
-    <div class="container">
-        <ul class="nav navbar-nav top_nav">
-            <li><a href="#"><i class="fa fa-envelope-o"></i>contact@sup-connexion.com</a></li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-clock-o"></i>
-                    <span id="date_heure"></span>
-                </a>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right social_nav">
-            <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-            <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-            <div class="collapse in" id="collapseExample">
-                <ul class="nav">
-                    <li>
-                        <a href="#profile">
-                            <span class="link-collapse">My Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#edit">
-                            <span class="link-collapse">Edit Profile</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#settings">
-                            <span class="link-collapse">Settings</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </ul>
-        <ul class="nav navbar-nav navbar-right" style="margin-right: -350px">
-            <li><a type="button" href="locale/fr" class="btn-image white-text" style="background-image: url('users/images/fr.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px; margin-right: 5px"></a></li>
-            <li><a type="button" href="locale/en" class="btn-image white-text" style="background-image: url('users/images/en.png') !important; width: 60px; height: 20px; background-size: cover; margin-top: 10px"></a></li>
-        </ul>
-    </div>
-</section>
-
+    @include('parts/first_section')
 
 	<!-- Header_Area -->
     <nav class="navbar navbar-default header_aera" id="main_navbar">
